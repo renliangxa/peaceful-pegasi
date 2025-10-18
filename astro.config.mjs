c8979d77e31f48835f0658f87fe6be3694918d08
renliang <renliang@handi.com>
2025-10-18 08:45:55 +0800

    edit site

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://stupendous-puppy-c98b78.netlify.app/',
 	integrations: [mdx(), sitemap()],
 });
